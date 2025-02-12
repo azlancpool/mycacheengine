de7814265c5103aa287bbd78cd88da1758867805
Cristian Naranjo <cnaranjo@payvalida.com>
2025-02-12 06:33:35 -0500

    Changes: - Modify hashKeyToInt fixing the error got for different data types provided. - Added more unit tests scenarios in order to validate possible edge cases. - Marked as done tasks related error when two equivalent inputs with different data types returns the same value.

--- service/api.go
+++ service/api.go
@@ -6,8 +6,9 @@ import (
 )
 
 // hashKeyToInt converts a given key of any comparable type into a hashed integer value.
+// For this it get the string equivalent to concatenation of the key value and data type.
 func hashKeyToInt[K comparable](key K) int {
 	hasher := fnv.New32a()
-	hasher.Write([]byte(fmt.Sprintf("%v", key)))
+	hasher.Write([]byte(fmt.Sprintf("%[1]v%[1]T", key)))
 	return int(hasher.Sum32())
 }
--- service/api_test.go
+++ service/api_test.go
@@ -19,34 +19,34 @@ var _ = Describe("testing hashKeyToInt functionality", hashKeyToIntTest)
 func hashKeyToIntTest() {
 	When("Testing int as input values", func() {
 		Context("Given 1 (int) as input", func() {
-			It("should return '873244444'", func() {
-				Expect(hashKeyToInt(1)).Should(Equal(873244444))
+			It("should return '1645622373'", func() {
+				Expect(hashKeyToInt(1)).Should(Equal(1645622373))
 			})
 		})
 
 		Context("Given 2 (int) as input", func() {
-			It("should return '923577301'", func() {
-				Expect(hashKeyToInt(2)).Should(Equal(923577301))
+			It("should return '517158638'", func() {
+				Expect(hashKeyToInt(2)).Should(Equal(517158638))
 			})
 		})
 
 		Context("Given a maxInt value as input", func() {
-			It("should return '546809323'", func() {
-				Expect(hashKeyToInt(math.MaxInt)).Should(Equal(546809323))
+			It("should return '2756140996'", func() {
+				Expect(hashKeyToInt(math.MaxInt)).Should(Equal(2756140996))
 			})
 		})
 	})
 
 	When("Testing case sensitive cases for string data type", func() {
 		Context("Given 'foo' (string) as input", func() {
-			It("should return '2851307223'", func() {
-				Expect(hashKeyToInt("foo")).Should(Equal(2851307223))
+			It("should return '2038559810'", func() {
+				Expect(hashKeyToInt("foo")).Should(Equal(2038559810))
 			})
 		})
 
 		Context("Given 'FOO' (string) as input", func() {
-			It("should return '3972896247'", func() {
-				Expect(hashKeyToInt("FOO")).Should(Equal(3972896247))
+			It("should return '3571895650'", func() {
+				Expect(hashKeyToInt("FOO")).Should(Equal(3571895650))
 			})
 		})
 
@@ -58,12 +58,34 @@ func hashKeyToIntTest() {
 		})
 	})
 
-	// TODO: Implement functionality that allows to meet this test scenario
 	When("Testing same input value, but with different data type", func() {
 		Context("Given '1' (int) as input and '1' (string)", func() {
 			It("should return FALSE if compare their results", func() {
-				result := hashKeyToInt("1") == hashKeyToInt(1)
-				Expect(result).Should(BeFalse())
+				comparissionResult := hashKeyToInt("1") == hashKeyToInt(1)
+				Expect(comparissionResult).Should(BeFalse())
+			})
+		})
+
+		Context("Given 'true' (string) as input and 'true' (bool)", func() {
+			It("should return FALSE if compare their results", func() {
+				comparissionResult := hashKeyToInt("true") == hashKeyToInt(true)
+				Expect(comparissionResult).Should(BeFalse())
+			})
+		})
+	})
+
+	When("Testing same input value and same data type", func() {
+		Context("Given the same input for string data type in different calls", func() {
+			It("should return TRUE if compare their results", func() {
+				comparissionResult := hashKeyToInt("foo") == hashKeyToInt("foo")
+				Expect(comparissionResult).Should(BeTrue())
+			})
+		})
+
+		Context("Given the same input for int data type in different calls", func() {
+			It("should return TRUE if compare their results", func() {
+				comparissionResult := hashKeyToInt(123) == hashKeyToInt(123)
+				Expect(comparissionResult).Should(BeTrue())
 			})
 		})
 	})
